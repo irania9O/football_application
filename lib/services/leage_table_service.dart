788520ecaefb5d1b3b3e645a4884d6ef5a201734
Seyed Ali <wwwwwq37@gmail.com>
2023-04-16 22:04:14 +0330

    add table service.

---
+++ lib/services/leage_table_service.dart
@@ -0,0 +1,19 @@
+import 'package:app/models/league_table.dart';
+import 'package:http/http.dart' as http;
+
+class LeagueTableService {
+  Future<Table?> fetchLeagueTable() async {
+    var client = http.Client();
+
+    var url =
+        Uri.parse('https://api.footballi.net/api/v2/match/tab/?version=2');
+    var response = await client.get(url);
+    // print(response.statusCode);
+    if (response.statusCode == 200) {
+      var jsonString = response.body;
+      return tableFromJson(jsonString);
+    } else {
+      return null;
+    }
+  }
+}
