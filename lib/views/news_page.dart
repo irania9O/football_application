b0e960a8ca01c4227dc6d3e06142e7ccc66d35ae
Seyed Ali <wwwwwq37@gmail.com>
2023-04-19 16:36:17 +0330

    show news.

--- lib/views/news_page.dart
+++ lib/views/news_page.dart
@@ -34,7 +34,44 @@ class _NewsPageState extends State<NewsPage> {
   @override
   Widget build(BuildContext context) {
     return isLoaded
-        ? const Text("News")
+        ? ListView.builder(
+            itemCount: NewsData.data.news.length,
+            itemBuilder: (context, index) {
+              return Card(
+                child: ListTile(
+                  leading: Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey,
+                      image: DecorationImage(
+                        image: NetworkImage(
+                          NewsData.data.news[index].newsImage[0].url,
+                        ),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  title: Padding(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    child: Text(
+                      NewsData.data.news[index].title,
+                      textAlign: TextAlign.right,
+                      textDirection: TextDirection.rtl,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  subtitle: Text(NewsData.data.news[index].summary,
+                      maxLines: 1, overflow: TextOverflow.ellipsis),
+                  contentPadding: const EdgeInsets.all(12),
+                ),
+              );
+            },
+          )
         : const Center(child: CircularProgressIndicator());
   }
 }
