6ca99764c33fec2df401f92468d0fcc9ed68f1e5
Seyed Ali <wwwwwq37@gmail.com>
2023-04-20 13:27:30 +0330

    add load more to news page and new view.

--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -1,9 +1,10 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-Future<Map<String, dynamic>?> fetchNews(id) async {
+Future<Map<String, dynamic>?> fetchNews(cursor) async {
   var client = http.Client();
-  var url = Uri.parse('https://api.footballi.net/api/v2/news/latest?version=2');
+  var url = Uri.parse(
+      'https://api.footballi.net/api/v2/news/latest?version=2&cursor=$cursor');
   var response = await client.get(url);
   if (response.statusCode == 200) {
     var jsonString = response.body;
--- lib/views/news_page.dart
+++ lib/views/news_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shamsi_date/shamsi_date.dart';
 
 import '../services/news_service.dart';
 
@@ -13,22 +14,45 @@ class NewsPage extends StatefulWidget {
 class _NewsPageState extends State<NewsPage> {
   // ignore: prefer_typing_uninitialized_variables, non_constant_identifier_names
   var NewsData;
+  int cursor = DateTime.now().millisecondsSinceEpoch ~/ 1000;
+  final controller = ScrollController();
   bool isLoaded = false;
 
   @override
   void initState() {
     super.initState();
+    controller.addListener(() {
+      if (controller.position.pixels == controller.position.maxScrollExtent) {
+        fetchMoreData();
+      }
+    });
     fetchFirstData();
   }
 
   fetchFirstData() async {
-    NewsData = await fetchNews(100);
+    NewsData = await fetchNews(cursor);
     if (NewsData != null) {
       setState(() {
-        print(NewsData['data']['news'].length);
+        cursor = int.parse(NewsData['meta']['cursor']);
         isLoaded = true;
       });
     }
+  }
+
+  fetchMoreData() async {
+    var moreData = await fetchNews(cursor);
+    if (moreData != null) {
+      setState(() {
+        cursor = int.parse(moreData['meta']['cursor']);
+        NewsData['data']['news'].addAll(moreData['data']['news']);
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -38,20 +62,34 @@ class _NewsPageState extends State<NewsPage> {
             behavior:
                 ScrollConfiguration.of(context).copyWith(scrollbars: false),
             child: ListView.builder(
-              itemCount: NewsData['data']['news'].length,
+              controller: controller,
+              itemCount: NewsData['data']['news'].length + 1,
               itemBuilder: (context, index) {
-                if (NewsData['data']['news'][index]['language'] != 1) {
-                  return const SizedBox();
-                }
-                return Card(
-                  child: ListTile(
-                    leading: Container(
-                      width: 100,
-                      height: 100,
+                if (index < NewsData['data']['news'].length) {
+                  if (NewsData['data']['news'][index]['language'] != 1) {
+                    return const SizedBox();
+                  }
+                  JalaliFormatter timepost = Jalali.fromDateTime(
+                    DateTime.fromMillisecondsSinceEpoch(
+                      NewsData['data']['news'][index]['updated_at'] * 1000,
+                    ),
+                  ).formatter;
+                  return InkWell(
+                    highlightColor: Colors.deepOrangeAccent,
+                    hoverColor: Colors.deepOrange,
+                    splashColor: Colors.white,
+                    // onTap: () {
+                    //   print(competitions[index].homeTeam.nameEn);
+                    // },
+                    autofocus: false,
+                    child: Container(
+                      margin: const EdgeInsets.all(10),
+                      padding: const EdgeInsets.all(15),
+                      height: 150,
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(12),
                         image: DecorationImage(
+                          opacity: 0.1,
                           image: NetworkImage(
                             NewsData['data']['news'][index]['news_image'][0]
                                 ['url'],
@@ -59,24 +97,78 @@ class _NewsPageState extends State<NewsPage> {
                           fit: BoxFit.cover,
                         ),
                       ),
+                      foregroundDecoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                          color: Colors.grey.withOpacity(0.5),
+                        ),
+                      ),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 20),
+                                  child: Text(
+                                    NewsData['data']['news'][index]['title'],
+                                    style: const TextStyle(fontSize: 16),
+                                    maxLines: 3,
+                                    textAlign: TextAlign.start,
+                                    overflow: TextOverflow.ellipsis,
+                                    textDirection: TextDirection.rtl,
+                                    textWidthBasis: TextWidthBasis.parent,
+                                  ),
+                                ),
+                                const Spacer(),
+                                Text(
+                                  "${timepost.d} ${timepost.mN} ${timepost.y}",
+                                  style: const TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.grey,
+                                  ),
+                                  textAlign: TextAlign.start,
+                                  textDirection: TextDirection.rtl,
+                                  textWidthBasis: TextWidthBasis.parent,
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(width: 20),
+                          Container(
+                            width: 80,
+                            height: 80,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              image: DecorationImage(
+                                image: NetworkImage(
+                                  NewsData['data']['news'][index]['news_image']
+                                      [0]['url'],
+                                ),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                    title: Padding(
-                      padding: const EdgeInsets.only(bottom: 10),
-                      child: Text(
-                        NewsData['data']['news'][index]['title'],
-                        textAlign: TextAlign.right,
-                        textDirection: TextDirection.rtl,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
+                  );
+                } else {
+                  return const SafeArea(
+                    child: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Center(
+                        child: CircularProgressIndicator(
+                          color: Colors.deepOrangeAccent,
+                          strokeWidth: 7,
+                          valueColor: AlwaysStoppedAnimation<Color>(
+                              Colors.deepOrangeAccent),
                         ),
                       ),
                     ),
-                    subtitle: Text(NewsData['data']['news'][index]['summary'],
-                        maxLines: 1, overflow: TextOverflow.ellipsis),
-                    contentPadding: const EdgeInsets.all(12),
-                  ),
-                );
+                  );
+                }
               },
             ))
         : const Scaffold(
