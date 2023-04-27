e9b7e3375ce91e8f7a1e198304e402d0f14ef36b
Seyed Ali <wwwwwq37@gmail.com>
2023-04-27 23:38:24 +0330

    add page body.

--- lib/views/news_detail.dart
+++ lib/views/news_detail.dart
@@ -1,5 +1,8 @@
 import 'package:app/services/news_detail_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:html/dom.dart' as dom;
+import 'package:html/parser.dart';
 
 class NewsDetailPage extends StatefulWidget {
   final int newsId;
@@ -25,6 +28,9 @@ class _NewsDetailPageState extends State<NewsDetailPage> {
 
       if (NewData != null) {
         setState(() {
+          var document = parse(
+              '<body>Hello world! <a href="www.html5rocks.com">HTML5 rocks!');
+          print(document);
           isLoaded = true;
         });
       }
@@ -54,50 +60,93 @@ class _NewsDetailPageState extends State<NewsDetailPage> {
                         fit: BoxFit.cover,
                       ),
                     )),
-              ],
-              body: Column(
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(5),
-                    margin: const EdgeInsets.all(10),
-                    child: Text(
-                      NewData["data"]["details"]["title"],
-                      textAlign: TextAlign.start,
-                      textDirection: TextDirection.rtl,
-                      style: const TextStyle(
-                        fontSize: 18,
-                      ),
-                    ),
+                // Container(
+                //   padding: const EdgeInsets.all(5),
+                //   margin: const EdgeInsets.all(10),
+                //   child: Text(
+                //     NewData["data"]["details"]["title"],
+                //     textAlign: TextAlign.start,
+                //     textDirection: TextDirection.rtl,
+                //     style: const TextStyle(
+                //       fontSize: 18,
+                //     ),
+                //   ),
+                // ),
+                SliverAppBar(
+                  automaticallyImplyLeading: false,
+                  backgroundColor: Colors.white,
+                  toolbarHeight: 60,
+                  pinned: true,
+                  centerTitle: true,
+                  title: Text(
+                    "${NewData["data"]["details"]["title"]}",
+                    style: const TextStyle(
+                        color: Colors.deepOrangeAccent,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold),
                   ),
-                ],
+                )
+              ],
+
+              body: SingleChildScrollView(
+                child: Html(
+                  data: NewData["data"]["details"]["body"],
+                  style: {
+                    "body": Style(
+                      fontSize: const FontSize(18.0),
+                      textAlign: TextAlign.right,
+                      direction: TextDirection.rtl,
+                      padding: const EdgeInsets.all(10),
+                    ),
+                  },
+                ),
               ),
+              // body: Column(
+              //   children: [
+              //     Container(
+              //       padding: const EdgeInsets.all(5),
+              //       margin: const EdgeInsets.all(10),
+              //       child: Text(
+              //         NewData["data"]["details"]["title"],
+              //         textAlign: TextAlign.start,
+              //         textDirection: TextDirection.rtl,
+              //         style: const TextStyle(
+              //           fontSize: 18,
+              //         ),
+              //       ),
+              //     ),
+
+              //   ],
+              // ),
             ),
           )
         : Scaffold(
             body: Center(
-                child: Scaffold(
-                    appBar: AppBar(
-                        title: const Text('فوتبالو',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold)),
-                        backgroundColor: Colors.deepOrangeAccent,
-                        centerTitle: true,
-                        elevation: 10,
-                        toolbarHeight: 60,
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.vertical(
-                              bottom: Radius.elliptical(100, 100)),
-                        )),
-                    body: const Center(
-                      child: CircularProgressIndicator(
-                        color: Colors.deepOrangeAccent,
-                        strokeWidth: 7,
-                        valueColor: AlwaysStoppedAnimation<Color>(
-                            Colors.deepOrangeAccent),
-                      ),
-                    ))),
+              child: Scaffold(
+                appBar: AppBar(
+                    title: const Text('فوتبالو',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold)),
+                    backgroundColor: Colors.deepOrangeAccent,
+                    centerTitle: true,
+                    elevation: 10,
+                    toolbarHeight: 60,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.vertical(
+                          bottom: Radius.elliptical(100, 100)),
+                    )),
+                body: const Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.deepOrangeAccent,
+                    strokeWidth: 7,
+                    valueColor:
+                        AlwaysStoppedAnimation<Color>(Colors.deepOrangeAccent),
+                  ),
+                ),
+              ),
+            ),
           );
   }
 }
