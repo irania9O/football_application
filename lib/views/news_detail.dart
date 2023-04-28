0794b1c4c7b9316a56966007090ee4662c4aebe4
Seyed Ali <wwwwwq37@gmail.com>
2023-04-28 11:00:33 +0330

    add header, title news

--- lib/views/news_detail.dart
+++ lib/views/news_detail.dart
@@ -1,5 +1,6 @@
 import 'package:app/services/news_detail_service.dart';
 import 'package:flutter/material.dart';
+// import 'package:flutter_html/flutter_html.dart';
 
 class NewsDetailPage extends StatefulWidget {
   final int newsId;
@@ -10,6 +11,7 @@ class NewsDetailPage extends StatefulWidget {
 }
 
 class _NewsDetailPageState extends State<NewsDetailPage> {
+  // ignore: non_constant_identifier_names, prefer_typing_uninitialized_variables
   var NewData;
   bool isLoaded = false;
 
@@ -54,7 +56,47 @@ class _NewsDetailPageState extends State<NewsDetailPage> {
                         fit: BoxFit.cover,
                       ),
                     )),
+                // Container(
+                //   padding: const EdgeInsets.all(5),
+                //   margin: const EdgeInsets.all(10),
+                //   child: Text(
+                //     NewData["data"]["details"]["title"],
+                //     textAlign: TextAlign.start,
+                //     textDirection: TextDirection.rtl,
+                //     style: const TextStyle(
+                //       fontSize: 18,
+                //     ),
+                //   ),
+                // ),
+                SliverAppBar(
+                  automaticallyImplyLeading: false,
+                  backgroundColor: Colors.white,
+                  toolbarHeight: 60,
+                  pinned: true,
+                  centerTitle: true,
+                  title: Text(
+                    "${NewData["data"]["details"]["title"]}",
+                    style: const TextStyle(
+                        color: Colors.deepOrangeAccent,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold),
+                  ),
+                )
               ],
+
+              // body: SingleChildScrollView(
+              //   child: Html(
+              //     data: NewData["data"]["details"]["body"],
+              //     style: {
+              //       "body": Style(
+              //         fontSize: const FontSize(18.0),
+              //         textAlign: TextAlign.right,
+              //         direction: TextDirection.rtl,
+              //         padding: const EdgeInsets.all(10),
+              //       ),
+              //     },
+              //   ),
+              // ),
               body: Column(
                 children: [
                   Container(
@@ -75,29 +117,31 @@ class _NewsDetailPageState extends State<NewsDetailPage> {
           )
         : Scaffold(
             body: Center(
-                child: Scaffold(
-                    appBar: AppBar(
-                        title: const Text('فوتبالو',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold)),
-                        backgroundColor: Colors.deepOrangeAccent,
-                        centerTitle: true,
-                        elevation: 10,
-                        toolbarHeight: 60,
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.vertical(
-                              bottom: Radius.elliptical(100, 100)),
-                        )),
-                    body: const Center(
-                      child: CircularProgressIndicator(
-                        color: Colors.deepOrangeAccent,
-                        strokeWidth: 7,
-                        valueColor: AlwaysStoppedAnimation<Color>(
-                            Colors.deepOrangeAccent),
-                      ),
-                    ))),
+              child: Scaffold(
+                appBar: AppBar(
+                    title: const Text('فوتبالو',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold)),
+                    backgroundColor: Colors.deepOrangeAccent,
+                    centerTitle: true,
+                    elevation: 10,
+                    toolbarHeight: 60,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.vertical(
+                          bottom: Radius.elliptical(100, 100)),
+                    )),
+                body: const Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.deepOrangeAccent,
+                    strokeWidth: 7,
+                    valueColor:
+                        AlwaysStoppedAnimation<Color>(Colors.deepOrangeAccent),
+                  ),
+                ),
+              ),
+            ),
           );
   }
 }
